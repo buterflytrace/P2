26aab9757efe700de58bcd07785ce8c06d9a7497
Tracy Weimer <tracyw@uoregon.edu>
2015-04-14 17:01:26 -0700

    uploading baseline files to fix formatting

---
+++ javascripts/javascript.js
@@ -0,0 +1,10 @@
+javascript.js
+var isArmstrong(n){
+	"use strict";
+	var sum=0
+	//convert to a string//
+	n = String(n);
+	//traverse n//
+	for (var i = 0; i < n.length ; ++i)
+		sum = sum + math.pow(n[i], n.length);
+}
